cf297b03bfd5bd6a5f74e33189a56153cf93b0e8
Daniel <daniel.castelline@gmail.com>
2021-02-08 18:53:56 -0600

    created burgers_controller.js in controllers folder, imported express and burger.js, outline comments, exported router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+// Require express and burger.js
+const express = require('express');
+const burger = require('./models/burger.js');
+
+// CREATE ROUTER
+
+// Export router
+module.exports = router;
