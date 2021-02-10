53b8b0dec17271a3d84dc89003e1bcdb1a130a00
Daniel <daniel.castelline@gmail.com>
2021-02-09 20:24:51 -0600

    added router code to burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,42 @@
 // Require express and burger.js
 const express = require('express');
-const burger = require('./models/burger.js');
+const burger = require('../models/burger.js');
 
 // CREATE ROUTER
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burger: data,
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/api/burger', (req, res) => {
+    burger.create(['name', 'devoured'], [req.body.name, req.body.devoured], (result) =>{
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put('/api/burger/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+    console.log('condition', condition);
+    burger.update(
+        {
+            devoured: req.body.devoured,
+        },
+        condition,
+        (result) => {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
 
 // Export router
 module.exports = router;
