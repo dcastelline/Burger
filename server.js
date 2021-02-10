03f5f76ab09a76798783370a6ee312624042d2af
Daniel <daniel.castelline@gmail.com>
2021-02-09 21:05:49 -0600

    realized this won't work if server.js is empty, so I filled it out with requirements and listeners

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 // Require express
 const express = require('express');
+
+// Define PORT
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middleware for handling the JSON and for static content from public
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set handlebars
+const exphbs = require('express-handlebars');
+const router = require('./controllers/burgers_controller.js');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, () =>
+    console.log(`Server listening on: http://localhost:${PORT}`)
+);
