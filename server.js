016d12cc5a1054170ab4718c91952a152f339218
Daniel <daniel.castelline@gmail.com>
2021-02-09 22:59:35 -0600

    removed duplicate routes requiring in server.js

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Middleware for handling the JSON and for static content from public
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set handlebars
 const exphbs = require('express-handlebars');
-const router = require('./controllers/burgers_controller.js');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
