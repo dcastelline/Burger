ea09895cb39de38868fea35f89443497753bdc09
Daniel <daniel.castelline@gmail.com>
2021-02-09 20:13:22 -0600

    wrote functions in burger.js to call orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,20 @@
 // Require ORM
-const orm = require('orm');
+const { selectAll } = require('../config/orm.js');
+const orm = require('../config/orm.js');
 
 // CREATE CODE THAT WILL CALL ORM FUNCTIONS USING BURGER SPECIFIC INPUT FOR THE ORM
+const burger = {
+    all(cb) {
+      orm.selectAll('burgers', (res) => cb(res));
+    },
+    // The variables cols and vals are arrays.
+    create(cols, vals, cb) {
+      orm.insertOne('cats', cols, vals, (res) => cb(res));
+    },
+    update(objColVals, condition, cb) {
+      orm.updateOne('cats', objColVals, condition, (res) => cb(res));
+    },
+  };
 
 // Export burger.js
 module.exports = burger;
